4806a10c3ccc53623189a9845d63089f1ee098dc
Adam Venton <asventon1@gmail.com>
2018-08-31 18:05:45 -0500

    Make SFML Window

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ SRCDIR = src
 all: Snake
 
 Snake: $(OBJDIR)/main.o
-	g++ $(OBJDIR)/main.o -o Snake
+	g++ $(OBJDIR)/main.o -o Snake -lsfml-graphics -lsfml-window -lsfml-system
 
 $(OBJDIR)/main.o: $(SRCDIR)/main.cpp
 	g++ -c $(SRCDIR)/main.cpp -o $(OBJDIR)/main.o
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
-using namespace std;
+#include <SFML/Graphics.hpp>
 
-int main(){
-	cout << "moo\n";
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(512, 512), "SFML Tutorial", sf::Style::Close | sf::Style::Titlebar);
+
+	while(window.isOpen()){
+
+		sf::Event evnt;
+
+		while(window.pollEvent(evnt)){
+			if(evnt.type == evnt.Closed){
+				window.close();
+			}
+		}	
+
+	}
+
 	return 0;
 }
